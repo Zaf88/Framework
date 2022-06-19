b9f2f864cdcf23d540400f0f1c06d2aa5c78e67a
Your Name <applezafar@gmail.com>
2022-06-19 20:39:29 +1000

    homework framework

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(1*i);
+            System.out.println();
+            int b =0;
+            for (int i1 = 2; i <=10 ; i++) {
+                System.out.println(2*i);
+                System.out.println();
+                int c =0;
+                for (int i2 = 3; i <=10 ; i++) {
+                    System.out.println(3*i);
+
+                }
+
+            }
+
+
+        }
+    }
+
+}
