b59d2e39851daebe4306f6e3f251c55c1f16a3a5
Your Name <applezafar@gmail.com>
2022-07-17 23:52:29 +1000

    homework framework

--- src/Main.java
+++ src/Main.java
@@ -17,48 +17,39 @@ public class Main {
     final static String regex = "^[Сс]трада(.*)";
 
     public static void main(String[] args) throws IOException {
-        int a = 0;
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(1 * i);
-            System.out.println();
 
-            int b = 0;
-            for (int i1 = 2; i <= 10; i++) {
-                System.out.println(2 * i);
-                System.out.println();
-
-                int c = 0;
-                for (int i2 = 3; i <= 10; i++) {
-                    System.out.println(3 * i);
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++) {
+                System.out.println((i + 1) * (j + 1));
 
 
-        File f1 = new File("C:\\Users\\user\\IdeaProjects\\Framework\\voyna.txt");
-        String[] words;
-        String line = "страдание";
-        FileReader fr = new FileReader(f1);
-        BufferedReader br = new BufferedReader(fr);
-        String s;
+                File f1 = new File("C:\\Users\\user\\IdeaProjects\\Framework\\voyna.txt");
+                String[] words;
+                String line = "страдание";
+                FileReader fr = new FileReader(f1);
+                BufferedReader br = new BufferedReader(fr);
+                String s;
 
-        int count = 0;
-        while ((s = br.readLine()) != null) {
-            words = s.split(" ");
-            for (String word : words) {
+                int count = 0;
+                while ((s = br.readLine()) != null) {
+                    words = s.split(" ");
+                    for (String word : words) {
 
 
-                Pattern input = Pattern.compile("страда\\D", Pattern.CASE_INSENSITIVE);
-                Matcher mc = input.matcher(word);
-                while (mc.find())
-                    count++;
+                        Pattern input = Pattern.compile("страда\\D", Pattern.CASE_INSENSITIVE);
+                        Matcher mc = input.matcher(word);
+                        while (mc.find())
+                            count++;
+                    }
                 }
-            }
-            if (count != 0) {
-                System.out.println("The word is present" + count + "times in the file");
-            } else {
-                System.out.println("The word is not  present in the file");
-            }
-            fr.close();}}
 
-
+                if (count != 0) {
+                    System.out.println("The word is present" + count + "times in the file");
+                } else {
+                    System.out.println("The word is not  present in the file");
+                }
+                fr.close();
+            }
     File file = new File("voyna.txt");
     Parser parser = new Parser();
     ArrayList<String> lines = parser.parse(file);
@@ -68,5 +59,4 @@ public class Main {
     for (String line:lines) {
         if (pattern.matcher(line).find()) {
             System.out.println(line);
-        }}}}}
-
+        }}}}
