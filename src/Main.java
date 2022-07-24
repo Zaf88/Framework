466aa05ff3de4ada245ea61bc12d967468e029b1
Your Name <applezafar@gmail.com>
2022-07-24 20:54:04 +1000

    таблица умножения до 100

--- src/Main.java
+++ src/Main.java
@@ -13,54 +13,14 @@ import java.util.regex.Matcher;
 
 
 public class Main {
+
     final static String pattern = "страдание";
     final static String regex = "^[Сс]трада(.*)";
 
     public static void main(String[] args) throws IOException {
-
         for (int i = 0; i < 10; i++)
             for (int j = 0; j < 10; j++) {
                 System.out.println((i + 1) * (j + 1));
 
-
-                File f1 = new File("C:\\Users\\user\\IdeaProjects\\Framework\\voyna.txt");
-                String[] words;
-                String line = "страдание";
-                FileReader fr = new FileReader(f1);
-                BufferedReader br = new BufferedReader(fr);
-                String s;
-
-                int count = 0;
-                while ((s = br.readLine()) != null) {
-                    words = s.split(" ");
-                    for (String word : words) {
-
-
-                        Pattern input = Pattern.compile("страда\\D", Pattern.CASE_INSENSITIVE);
-                        Matcher mc = input.matcher(word);
-                        while (mc.find())
-                            count++;
-                    }
-                }
-
-                if (count != 0) {
-                    System.out.println("The word is present" + count + "times in the file");
-                } else {
-                    System.out.println("The word is not  present in the file");
-                }
-                fr.close();
-            }
-                File file = new File("voyna.txt");
-                Parser parser = new Parser();
-                ArrayList<String> lines = parser.parse(file);
-
-                Pattern pattern = Pattern.compile(regex);
-
-                for (String line : lines) {
-                    if (pattern.matcher(line).find()) {
-                        System.out.println(line);
-                    }
-                }
-            }
-    }
+            }}}
 
